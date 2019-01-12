ec2b7bae968263b42aadd3046a60fa0f977843eb
tikavalery <43121065+tikavalery@users.noreply.github.com>
2019-01-12 01:57:29 -0800

    connection changes

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+   connection = mysql.createConnection({
+    //port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
